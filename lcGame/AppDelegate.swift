24d0cc7840140cf464211a71c036c592e108c2dc
LCcccccc <liuchao@liuchaodeMacBook-Pro.local>
2015-08-25 22:47:56 +0800

    change3

--- lcGame/AppDelegate.swift
+++ lcGame/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var change1:Int!
     var change2:Int!
+    var change3:Int!
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
